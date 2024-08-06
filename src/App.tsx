d8fe6cb2153258ab155442927738f86509afb3ae
atnuhs <syunnta0033@gmail.com>
2024-08-06 20:53:44 +0900

    feat: Create thread list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Post = {
+  id: string;
+  title: string;
+};
+
+const App = () => {
+  const [data, setData] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          "https://railway.bulletinboard.techtrain.dev/threads"
+        );
+        const result = await res.json();
+        setData(result);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
+        <h2 className="text-2xl font-bold border-b p-4">新着スレッド</h2>
+        <div className="bg-white  shadow-lg max-w-3xl w-full">
+          <div className="">
+            {data.map((post: Post) => (
+              <div
+                key={post.id}
+                className="bg-white shadow-sm  p-4 border-b border-gray-300"
+              >
+                <h2 className="text-lg font-medium text-gray-900">
+                  {post.title}
+                </h2>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
